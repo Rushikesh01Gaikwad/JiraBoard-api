c0897cdf3e5af5de54fa32d5cca4893d5a4dc578
Rushikesh Gaikwad <rushikeshhtc@gmail.com>
2025-07-26 08:32:22 +0530

    data in pro

--- Context/DataContext.cs
+++ Context/DataContext.cs
@@ -5,6 +5,7 @@ namespace JiraBoard_api.Context
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }   
         public DbSet<projectData> projects { get; set; }
     }
 }
